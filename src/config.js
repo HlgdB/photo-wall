2cc0f8dc7ec1ce29a4d94943d3ff9e915afa7cd6
Renbin_Hu <1297693955@qq.com>
2022-02-01 03:21:22 +0800

    Update config.js
    change to ip

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://changyang.love";
+export const BACKEND_URL = "http://150.158.152.148";
 // export const BACKEND_URL = "http://localhost:8888";
 
-export const Destination = 'http://changyang.love:8889/uploads/';
+export const Destination = 'http://150.158.152.148:8889/uploads/';
